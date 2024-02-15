f2289bd2481ea5ec883dae046fef1a35a3541500
Lukas.J.Han <lukas.j.han@gmail.com>
2024-02-15 23:34:10 +0900

    feat: support webp

--- js/index.js
+++ js/index.js
@@ -14,32 +14,39 @@ font
     document.body.style.opacity = 1;
     new Typed('.typedTitle', options);
   })
-  .catch(err => (document.body.style.opacity = 1));
+  .catch((err) => (document.body.style.opacity = 1));
 
-const fileTypeCheck = fileName => {
+const fileTypeCheck = (fileName) => {
   const pathpoint = fileName.lastIndexOf('.');
 
   const filepoint = fileName.substring(pathpoint + 1, fileName.length);
 
   const filetype = filepoint.toLowerCase();
 
-  if (!(filetype == 'jpg' || filetype == 'png' || filetype == 'jpeg')) {
-    alert('jpg, jpeg, png  확장자만 가능합니다.');
+  if (
+    !(
+      filetype == 'jpg' ||
+      filetype == 'png' ||
+      filetype == 'jpeg' ||
+      filetype == 'webp'
+    )
+  ) {
+    alert('jpg, jpeg, png, webp 확장자만 가능합니다.');
     return false;
   }
   return true;
 };
 
-const onChange = e => {
+const onChange = (e) => {
   const file = e.target.files[0];
   if (fileTypeCheck(file.name)) {
     const mainWrapper = document.querySelector('.mainWrapper');
     mainWrapper.classList.add('hide');
     const reader = new FileReader();
-    reader.onload = e => {
+    reader.onload = (e) => {
       const image = new Image();
       image.src = e.target.result;
-      image.onload = img => {
+      image.onload = (img) => {
         new Canvas(img.target);
       };
     };
